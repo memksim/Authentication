8ed155cdb6b270fab05a5ff25d374aa285ee2c8a
memksim <memksim@vk.com>
2022-05-19 20:28:27 +0500

    Сделал дизайн полей регистрации

--- app/src/main/java/com/memksim/authentication/MainActivity.kt
+++ app/src/main/java/com/memksim/authentication/MainActivity.kt
@@ -2,10 +2,22 @@ package com.memksim.authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.memksim.authentication.view.fragments.AuthPageFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null){
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer, AuthPageFragment())
+                .commit()
+        }
     }
+
+
+
 }
---
+++ app/src/main/java/com/memksim/authentication/view/fragments/AuthPageFragment.kt
@@ -0,0 +1,24 @@
+package com.memksim.authentication.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.memksim.authentication.R
+
+class AuthPageFragment: Fragment(R.layout.fragment_auth_page) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
